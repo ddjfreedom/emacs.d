32d949562636d45a9d224b0daa10d32f84dc26cf
ddjfreedom <ddj1990@gmail.com>
2012-11-28 20:36:20 -0500

    added update zenburn to custom-safe-theme

--- init.el
+++ init.el
@@ -61,7 +61,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("be7eadb2971d1057396c20e2eebaa08ec4bfd1efe9382c12917c6fe24352b7c1" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" "b7553781f4a831d5af6545f7a5967eb002c8daeee688c5cbf33bf27936ec18b3" default)))
+ '(custom-safe-themes (quote ("bf7ed640479049f1d74319ed004a9821072c1d9331bc1147e01d22748c18ebdf" "be7eadb2971d1057396c20e2eebaa08ec4bfd1efe9382c12917c6fe24352b7c1" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" "b7553781f4a831d5af6545f7a5967eb002c8daeee688c5cbf33bf27936ec18b3" default)))
  '(org-agenda-files (quote ("~/Documents/org/capture.org" "~/Documents/org/books.org" "~/Documents/org/todo.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
